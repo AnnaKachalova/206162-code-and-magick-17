145a505eb4ea9d06a24c63f459524fc8935486e8
Anna Kachalova <kachalova@uchi.ru>
2019-06-11 22:30:52 +0300

    module3-task1

---
+++ js/setup.js
@@ -0,0 +1,68 @@
+'use strict';
+// show block
+document.querySelector('.setup').classList.remove('hidden');
+
+var WIZARD_NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+];
+var WIZARD_LAST_NAMES = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг',
+];
+var COAT_COLORS = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)',
+];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var similarCharacters = [];
+
+var getRandElement = function (arr) {
+  var rand = Math.floor(Math.random() * arr.length);
+  return arr[rand];
+};
+
+for (var i = 0; i < 4; i++) {
+  var character = {
+    name: getRandElement(WIZARD_NAMES) + '' + getRandElement(WIZARD_LAST_NAMES),
+    coatColor: getRandElement(COAT_COLORS),
+    eyesColor: getRandElement(EYES_COLORS),
+  };
+  similarCharacters.push(character);
+}
+
+var similarListElement = document.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document
+  .querySelector('#similar-wizard-template')
+  .content.querySelector('.setup-similar-item');
+
+for (var i = 0; i < 4; i++) {
+  var currentWizard = similarCharacters[i];
+  var element = similarWizardTemplate.cloneNode(true);
+
+  element.querySelector('.setup-similar-label').textContent = currentWizard.name;
+  element.querySelector('.wizard-coat').style.fill = currentWizard.coatColor;
+  element.querySelector('.wizard-eyes').style.fill = currentWizard.eyesColor;
+
+  similarListElement.appendChild(element);
+}
+
+document.querySelector('.setup-similar').classList.remove('hidden');
