6f9c246560a63ef014b0af11fe246653b27babb1
Anna Kachalova <kachalova@uchi.ru>
2019-07-01 21:57:47 +0300

    m7_t1 fix2

--- js/util.js
+++ js/util.js
@@ -1,26 +1,23 @@
 'use strict';
 
-(function() {
+(function () {
   var ESC_KEYCODE = 27;
   var ENTER_KEYCODE = 13;
   window.util = {
-    isEscEvent: function(evt, action) {
+    isEscEvent: function (evt, action) {
       if (evt.keyCode === ESC_KEYCODE) {
         action();
       }
     },
-
-    isEnterEvent: function(evt, action) {
+    isEnterEvent: function (evt, action) {
       if (evt.keyCode === ENTER_KEYCODE) {
         action();
       }
     },
-    
-    getRandomNum: function(min, max) {
+    getRandomNum: function (min, max) {
       return Math.random() * (max - min) + min;
     },
-
-    getRandomElement: function(arr) {
+    getRandomElement: function (arr) {
       var rand = Math.floor(Math.random() * arr.length);
       return arr[rand];
     },
